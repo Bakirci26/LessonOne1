782a0d9f93848c0b44f20dc3affbf6eadcd4aee0
Bakırcı <Bakrc@DESKTOP-C15THLB>
2023-07-23 16:20:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LessonOne1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //type safety _ tip güvenliği
+            //Do not repeat your self- Kendini tekrarlama
+            string kategoriEtiketi = "Kategori";
+
+            int ogrenciSayisi = 32000;
+
+            double FaizOrani = 1.45;
+
+            bool SistemGirisYapmısMi = false;
+
+            double DolarDun = 20.00;
+
+            double DolarBugün = 22.00;
+
+
+            if (SistemGirisYapmısMi == true)
+            {
+                Console.WriteLine("Sisteme Giris Yapildi");
+            }
+            else
+            {
+                Console.WriteLine("Sisteme Giris Yapilmadi");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+            if (DolarBugün > DolarDun)
+                {
+                    Console.WriteLine("Dolar Artmıs");
+                }
+
+                else if (DolarDun < DolarBugün)
+                {
+                    Console.WriteLine("Dolar Düsmüs");
+                }
+                else
+                {
+                    Console.WriteLine("Dolar Sabit");
+                } 
+            //Simule ettik için ,Eğitim için 
+        }
+    }
+}
